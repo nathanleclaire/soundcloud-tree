fad2bd1f4f333149a5520b79874882ad94540060
Nathan LeClaire <nathanleclaire@gmail.com>
2013-02-04 00:44:19 -0500

    Pretty print of a json request and more goodies, woot

--- app/soundclash.py
+++ app/soundclash.py
@@ -3,7 +3,16 @@
 from flask import Flask
 from flask import render_template
 
+import requests
+
+client_id = '293e5c53495a034defc93aa172ea00ac'
+
 app = Flask(__name__)
+
+@app.route('/favorite_songs/<string:user>')
+def favorite_songs(user):
+	favorites = requests.get('http://api.soundcloud.com/users/' + user + '/favorites.json', params={'client_id': client_id})
+	return render_template('index.html', favorites_json=favorites.json())	
 
 @app.route('/')
 def home():
